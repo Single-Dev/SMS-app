017c1b043987910e227b61d9e6eafafa594a0550
bekzodbek2006 <bekzodbekk2006@gmail.com>
2023-09-06 18:26:15 +0500

    a user can create the profile

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -19,3 +19,18 @@ class CustomUser(AbstractUser):
     class Meta:
         ordering = ['-id']
         # (-) yoki (+) modellar ro'yahti qanday tuzish keark ekanligiga ta'sir qiladi
+
+class Profile(models.Model):
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profile"
+    user = models.OneToOneField(CustomUser(), on_delete=models.CASCADE)
+    profile_photo = models.ImageField(default="profile/profile.jpg", upload_to="profile", null=True, blank=True)
+    bio = models.CharField(max_length=100, null=True, blank=True, default="")
+    instagram = models.CharField(max_length=150, null=True, blank=True)
+    twitter = models.CharField(max_length=150, null=True, blank=True)
+    github = models.CharField(max_length=150, null=True, blank=True)
+    facebook = models.CharField(max_length=150, null=True, blank=True)
+    website = models.CharField(max_length=150, null=True, blank=True)
+    def __str__(self):
+        return f"id: {self.id}, {self.user}"
