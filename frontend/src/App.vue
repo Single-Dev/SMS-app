86c71dd59fcd632e28fe936d76493a3f7d8d3aec
bekzodbek2006 <bekzodbekk2006@gmail.com>
2023-09-22 21:11:47 +0500

    added profile router and minor changes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,6 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/login">Login</router-link> |
     <router-link to="/sign-up">SignUp</router-link>
-    <button @click="getMe">Chekc</button>
   </nav>
   <router-view @Login="Login" @Signup="Signup" />
 </template>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/auth/Login.vue'
 import SignUpView from '../views/auth/SignUp.vue'
-
+import Profile from '../views/profile/Profile.vue'
 const routes = [
   {
     path: '/',
@@ -18,6 +18,11 @@ const routes = [
     path: '/sign-up',
     name: 'SignUp',
     component: SignUpView
+  },
+  {
+    path: '/@:username',
+    name: 'Profile',
+    component: Profile
   },
 ]
 
