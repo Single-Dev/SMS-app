8b4f62fe055bba8b7b15d1bf603e7ae55059f490
bekzodbek2006 <bekzodbekk2006@gmail.com>
2023-09-28 17:33:23 +0500

    bugs fixed

---
+++ frontend/src/components/navigation/nav.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <MDBNavbar dark bg="dark" position="top" >
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">E-Cash</a>
+                    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarColor02"
+                        aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarColor02">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item active">
+                                <a class="nav-link" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </MDBNavbar>
+    </div>
+</template>
+<script>
+import { MDBNavbar } from 'mdb-vue-ui-kit';
+
+export default {
+    components: {
+        MDBNavbar,
+    },
+};
+</script>
+<style lang="">
+    
+</style>
